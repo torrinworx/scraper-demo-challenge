22152d1a8c42585eedaf0d1dcb27b57c0403bded
Torrin Leonard <82110564+torrinworx@users.noreply.github.com>
2023-02-08 18:33:31 -0500

    Trouble shooting

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const port = process.env.PORT || 3000;
-App.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
