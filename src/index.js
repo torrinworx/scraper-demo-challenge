980fefb0a342782648f7420dd7605a7e40bfac04
Torrin Leonard <82110564+torrinworx@users.noreply.github.com>
2023-02-08 18:05:15 -0500

    Adding Heroku deployment requirements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
